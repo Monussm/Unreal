8b2b24273632a99a47f8c5719f2089d33f80f8b5
Monu Gupta <monug1513@gmail.com>
2023-04-23 15:09:15 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+let valueDisplays=document.getElementById("number");
+let interval=500;
+valueDisplays.array.forEach(valueDisplays => {
+    
+let startvalue=0;
+let endvalue=parseInt(valueDisplays.getElementById("number"))
+let duration=Math.floor(interval/endvalue);
+let counter=setInterval(function(){
+
+startvalue +=1;
+valueDisplays.textcontent=startvalue
+
+
+
+
+})
+
+
+
+
+});
